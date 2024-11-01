0b9904fb5743a04bbb2b4fc6dcbb979b8098ecf0
arunmadana <arunkumarmadana@gmail.com>
2024-11-01 15:42:40 +0530

    ui changed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header=() =>{
   return (
     <div className="header-container" style={{ position: "sticky", top: 0, backgroundColor: "#f8f9fa", zIndex: 100 ,color:"#4f0e83"}} >
         <div className="logo-container">
-           <img src={Logo} alt="logo" width="20%"   style={{marginLeft:"50px"}} />
+           <img src={Logo} alt="logo" width="28%"   style={{marginLeft:"50px"}} />
           
         </div>
       
